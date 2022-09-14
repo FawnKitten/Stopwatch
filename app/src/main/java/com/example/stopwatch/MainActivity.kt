5c377c4a26559b549c12e496d68d9bf91e03edf3
FawnKitten <joaopollo@gmail.com>
2022-09-14 10:43:44 -0700

    added functionality to the buttons and the chronometer.
    the start button when pressed:
    - becomes a stop button,
    - changes color to red, and
    - starts/resumes the chronometer.
    stop button when pressed:
    - becomes a start button
    - changes color to purple, and
    - stops the chronometer
    restart button restarts the timer to 0:00

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.stopwatch
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
 import android.util.Log
+import android.widget.Button
+import android.widget.Chronometer
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,10 +16,48 @@ class MainActivity : AppCompatActivity() {
         val TAG = "MainActivity"
     }
 
+    lateinit var startStop: Button
+    lateinit var chronometer: Chronometer
+    lateinit var reset: Button
+    var timeStopped: Long = 0
+
+    var running = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d(TAG, "onCreate: instance created")
+        wireWidgets()
+        setListeners()
+    }
+
+    private fun setListeners() {
+        startStop.setOnClickListener {
+            if (running) {
+                running = false
+                startStop.setBackgroundColor(Color.RED)
+                chronometer.base = SystemClock.elapsedRealtime() - timeStopped
+                chronometer.start()
+                startStop.text = "Stop"
+            } else {
+                running = true
+                startStop.setBackgroundColor(resources.getColor(R.color.purple_500))
+                timeStopped = SystemClock.elapsedRealtime() - chronometer.base
+                chronometer.stop()
+                startStop.text = "Start"
+            }
+        }
+
+        reset.setOnClickListener {
+            chronometer.base = SystemClock.elapsedRealtime()
+            timeStopped = 0
+        }
+    }
+
+    private fun wireWidgets() {
+        startStop = findViewById(R.id.button_main_start)
+        chronometer = findViewById(R.id.chronometer_main_stopwatch)
+        reset = findViewById(R.id.button_main_reset)
     }
 
     override fun onStart() {
